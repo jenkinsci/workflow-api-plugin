3587486837a762b5471798fbc5d9815aae682be8
Andrew Bayer <andrew.bayer@gmail.com>
2017-05-16 12:37:00 -0400

    Removed onPaused

--- src/main/java/org/jenkinsci/plugins/workflow/flow/FlowExecutionListener.java
+++ src/main/java/org/jenkinsci/plugins/workflow/flow/FlowExecutionListener.java
@@ -4,7 +4,7 @@ import hudson.ExtensionList;
 import hudson.ExtensionPoint;
 
 /**
- * Listens for significant status updates for a {@link FlowExecution}, such as started running, paused, or completed.
+ * Listens for significant status updates for a {@link FlowExecution}, such as started running or completed.
  *
  * @since 2.14
  * @author Andrew Bayer
@@ -17,14 +17,6 @@ public abstract class FlowExecutionListener implements ExtensionPoint {
      * @param execution The {@link FlowExecution} that has started running or resumed.
      */
     public void onRunning(FlowExecution execution) {
-    }
-
-    /**
-     * Called when a {@link FlowExecution} has paused.
-     *
-     * @param execution The {@link FlowExecution} that has paused.
-     */
-    public void onPaused(FlowExecution execution) {
     }
 
     /**
