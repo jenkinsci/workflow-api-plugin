a38459828777b435cfb3bac5b22502a72c6ee92d
Devin Nusbaum <d.w.nusbaum@gmail.com>
2022-01-14 17:29:41 -0500

    Revert "[JENKINS-67351] Avoid deadlock when resuming Pipelines in some cases (#188)"
    This reverts commit 6d6de2006ec5ec7d8e7dbf2b612ec9e5a887274c.

--- src/main/java/org/jenkinsci/plugins/workflow/flow/FlowExecutionList.java
+++ src/main/java/org/jenkinsci/plugins/workflow/flow/FlowExecutionList.java
@@ -10,7 +10,6 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 import hudson.ExtensionList;
 import hudson.XmlFile;
-import hudson.init.InitMilestone;
 import hudson.init.Terminator;
 import hudson.model.listeners.ItemListener;
 import hudson.remoting.SingleLaneExecutorService;
@@ -32,7 +31,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.kohsuke.accmod.Restricted;
-import org.kohsuke.accmod.restrictions.Beta;
 import org.kohsuke.accmod.restrictions.DoNotUse;
 
 /**
@@ -45,8 +43,6 @@ public class FlowExecutionList implements Iterable<FlowExecution> {
     private final CopyOnWriteList<FlowExecutionOwner> runningTasks = new CopyOnWriteList<>();
     private final SingleLaneExecutorService executor = new SingleLaneExecutorService(Timer.get());
     private XmlFile configFile;
-
-    private transient volatile boolean resumptionComplete;
 
     public FlowExecutionList() {
         load();
@@ -164,18 +160,6 @@ public class FlowExecutionList implements Iterable<FlowExecution> {
     }
 
     /**
-     * Returns true if all executions that were present in this {@link FlowExecutionList} have been loaded and resumed.
-     *
-     * This takes place slightly after {@link InitMilestone#COMPLETED} is reached during Jenkins startup.
-     *
-     * Useful to avoid resuming Pipelines in contexts that may lead to deadlock.
-     */
-    @Restricted(Beta.class)
-    public boolean isResumptionComplete() {
-        return resumptionComplete;
-    }
-
-    /**
      * When Jenkins starts up and everything is loaded, be sure to proactively resurrect
      * all the ongoing {@link FlowExecution}s so that they start running again.
      */
@@ -183,12 +167,10 @@ public class FlowExecutionList implements Iterable<FlowExecution> {
     public static class ItemListenerImpl extends ItemListener {
         @Override
         public void onLoaded() {
-            FlowExecutionList list = FlowExecutionList.get();
-            for (final FlowExecution e : list) {
+            for (final FlowExecution e : FlowExecutionList.get()) {
                 // The call to FlowExecutionOwner.get in the implementation of iterator() is sufficent to load the Pipeline.
                 LOGGER.log(Level.FINE, "Eagerly loaded {0}", e);
             }
-            list.resumptionComplete = true;
         }
     }
 
@@ -293,7 +275,7 @@ public class FlowExecutionList implements Iterable<FlowExecution> {
                     }
                 }
 
-            }, Timer.get()); // We always hold RunMap and WorkflowRun locks here, so we resume steps on a different thread to avoid potential deadlocks. See JENKINS-67351.
+            }, MoreExecutors.directExecutor()); // TODO: Unclear if we need to run this asynchronously or if StepExecution.onResume has any particular thread requirements.
         }
     }
 }
