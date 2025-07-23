3c9d50db3231cb18180213d8ec6e0f7abb148d0a
Julien Greffe <jgreffe@cloudbees.com>
2025-07-23 15:48:42 +0200

    Additional test

--- src/test/java/org/jenkinsci/plugins/workflow/log/tee/RemoteCustomFileLogStorageFactory.java
+++ src/test/java/org/jenkinsci/plugins/workflow/log/tee/RemoteCustomFileLogStorageFactory.java
@@ -1,27 +1,38 @@
 package org.jenkinsci.plugins.workflow.log.tee;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
 import java.io.File;
+import org.jenkinsci.Symbol;
 import org.jenkinsci.plugins.workflow.flow.FlowExecutionOwner;
+import org.jenkinsci.plugins.workflow.log.BrokenLogStorage;
 import org.jenkinsci.plugins.workflow.log.LogStorage;
 import org.jenkinsci.plugins.workflow.log.LogStorageFactory;
 import org.jenkinsci.plugins.workflow.log.LogStorageFactoryDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 public class RemoteCustomFileLogStorageFactory implements LogStorageFactory {
 
-    private final File file;
-
-    public RemoteCustomFileLogStorageFactory(File file) {
-        this.file = file;
-    }
+    @DataBoundConstructor
+    public RemoteCustomFileLogStorageFactory() {}
 
     @Override
     public LogStorage forBuild(@NonNull FlowExecutionOwner b) {
-        return RemoteCustomFileLogStorage.forFile(file);
+        try {
+            File file = new File(b.getRootDir(), "custom-log");
+            return RemoteCustomFileLogStorage.forFile(file);
+        } catch (Exception x) {
+            return new BrokenLogStorage(x);
+        }
     }
 
-    @Override
-    public LogStorageFactoryDescriptor<?> getDescriptor() {
-        return null;
+    @Extension
+    @Symbol("customLog")
+    public static final class DescriptorImpl extends LogStorageFactoryDescriptor<RemoteCustomFileLogStorageFactory> {
+        @NonNull
+        @Override
+        public String getDisplayName() {
+            return "Remote custom file log storage factory";
+        }
     }
 }
--- src/test/java/org/jenkinsci/plugins/workflow/log/tee/TeeLogStoragePipelineTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/log/tee/TeeLogStoragePipelineTest.java
@@ -1,16 +1,20 @@
 package org.jenkinsci.plugins.workflow.log.tee;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.io.FileMatchers.anExistingFile;
 
+import java.io.File;
+import java.nio.file.Files;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.log.FileLogStorageFactory;
 import org.jenkinsci.plugins.workflow.log.LogStorage;
 import org.jenkinsci.plugins.workflow.log.configuration.PipelineLoggingGlobalConfiguration;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 import org.jvnet.hudson.test.JenkinsRule;
 
 public class TeeLogStoragePipelineTest {
@@ -18,20 +22,27 @@ public class TeeLogStoragePipelineTest {
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
-    @Rule
-    public TemporaryFolder tmp = new TemporaryFolder();
-
     @Test
     public void smokes() throws Exception {
-        var storageFactory = new TeeLogStorageFactory(
-                new FileLogStorageFactory(), new RemoteCustomFileLogStorageFactory(tmp.newFile()));
+        var storageFactory =
+                new TeeLogStorageFactory(new FileLogStorageFactory(), new RemoteCustomFileLogStorageFactory());
         var config = PipelineLoggingGlobalConfiguration.get();
         config.setFactory(storageFactory);
 
         WorkflowJob workflowJob = j.createProject(WorkflowJob.class);
         workflowJob.setDefinition(new CpsFlowDefinition("echo 'Hello World'", true));
 
-        j.buildAndAssertSuccess(workflowJob);
+        WorkflowRun b = j.buildAndAssertSuccess(workflowJob);
         assertThat(LogStorage.of(workflowJob.getLastBuild().asFlowExecutionOwner()), instanceOf(TeeLogStorage.class));
+
+        File logIndex = new File(b.getRootDir(), "log-index");
+        assertThat(logIndex, anExistingFile());
+        File log = new File(b.getRootDir(), "log");
+        assertThat(log, anExistingFile());
+        assertThat(Files.readString(log.toPath()), containsString("Hello World"));
+
+        File customLog = new File(b.getRootDir(), "custom-log");
+        assertThat(customLog, anExistingFile());
+        assertThat(Files.readString(customLog.toPath()), containsString("Hello World"));
     }
 }
